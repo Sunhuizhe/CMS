c6cf03c09784ce5f5700e9cdcd9fc2c46f9b189c
Sun <Sun>
2017-06-23 16:21:48 +0800

    CMS作业提交文件夹

---
+++ www/firstCMS/phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost/firstCMS/',
+    'authkey' => 'bxO9p2liY9zW9V7xEqL8Gq0UpG8Chdpk',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
---
+++ www/hbschool/caches/error_log.php
@@ -0,0 +1,44 @@
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | array_keys() expects parameter 1 to be array, boolean given | phpcms\libs\functions\global.func.php | 1500
+<?php exit;?>05-27 16:44:02 | 2 | in_array() expects parameter 2 to be array, null given | phpcms\libs\functions\global.func.php | 1501
+<?php exit;?>05-27 16:44:02 | 2 | mysqli::mysqli(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) | phpcms\libs\classes\db_mysqli.class.php | 55
+<?php exit;?>05-27 16:44:02 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>05-27 16:44:02 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>05-27 16:44:02 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>05-27 16:44:02 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>05-27 16:44:02 | 2 | mysqli::close(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 409
+<?php exit;?>05-27 17:52:48 | 2 | Missing argument 2 for category::delete_child(), called in F:\wampsever\wamp\www\hbschool\phpcms\modules\admin\category.php on line 351 and defined | phpcms\modules\admin\category.php | 346
+<?php exit;?>06-01 10:28:58 | 2 | require(F:\wampsever\wamp\www\hbschool\phpcms\modules\content\fields\\config.inc.php): failed to open stream: No such file or directory | phpcms\modules\content\sitemodel_field.php | 207
+<?php exit;?>06-01 10:29:01 | 2 | require(F:\wampsever\wamp\www\hbschool\phpcms\modules\content\fields\\config.inc.php): failed to open stream: No such file or directory | phpcms\modules\content\sitemodel_field.php | 207
+<?php exit;?>06-01 10:29:18 | 2 | require(F:\wampsever\wamp\www\hbschool\phpcms\modules\content\fields\\config.inc.php): failed to open stream: No such file or directory | phpcms\modules\content\sitemodel_field.php | 207
+<?php exit;?>06-01 10:48:25 | 2 | require(F:\wampsever\wamp\www\hbschool\phpcms\modules\content\fields\\config.inc.php): failed to open stream: No such file or directory | phpcms\modules\content\sitemodel_field.php | 207
+<?php exit;?>06-01 10:52:52 | 2 | require(F:\wampsever\wamp\www\hbschool\phpcms\modules\content\fields\\config.inc.php): failed to open stream: No such file or directory | phpcms\modules\content\sitemodel_field.php | 207
+<?php exit;?>06-01 10:55:07 | 2 | require(F:\wampsever\wamp\www\hbschool\phpcms\modules\content\fields\\config.inc.php): failed to open stream: No such file or directory | phpcms\modules\content\sitemodel_field.php | 207
+<?php exit;?>06-01 11:05:04 | 2 | require(F:\wampsever\wamp\www\hbschool\phpcms\modules\content\fields\\config.inc.php): failed to open stream: No such file or directory | phpcms\modules\content\sitemodel_field.php | 207
